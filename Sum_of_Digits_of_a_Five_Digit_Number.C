5d337a615dc664f85a2a512700fa6c4f1b0a2ecb
core <146551153+Extinctione@users.noreply.github.com>
2025-10-27 00:37:45 +0530

    Create Sum_of_Digits_of_a_Five_Digit_Number.C

---
+++ Sum_of_Digits_of_a_Five_Digit_Number.C
@@ -0,0 +1,19 @@
+//https://www.hackerrank.com/challenges/sum-of-digits-of-a-five-digit-number/problem
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n;
+    int sum=0;
+    scanf("%d", &n);
+    if (n>= 10000 && n<= 99999){
+        while (n>0){
+            sum=sum+n%10;
+            n=n/10;}}
+        printf("%d \n", sum);
+    return 0;
+}
